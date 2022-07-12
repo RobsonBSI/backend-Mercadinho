f66ab9c8d422dc17d1da15305d911685e86cf9d0
RobsonBSI <103039603+RobsonBSI@users.noreply.github.com>
2022-07-12 17:07:27 -0300

    Delete backend-Mercadinho directory

--- backend-Mercadinho/src/main/java/com/example/backendmercadinho/Mercadinho/model/repository/ProdutoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.backendmercadinho.Mercadinho.model.repository;
-
-import com.example.backendmercadinho.Mercadinho.model.entity.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto,Long> {
-}
-
